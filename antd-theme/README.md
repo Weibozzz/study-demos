942a0bca1706eaa8308b5ee3f226b33e21f70ec9
Weibozzz <739291780@qq.com>
2018-06-08 10:18:20 +0800

    fix:修改react-antd-theme

--- antd-theme/README.md
+++
@@ -1,8 +0,0 @@
-## antd-theme
-展示了antd和create-react-app修改默认端口和主题颜色
-```
-cnpm i 
-npm start
-
-
-```
--- react-antd-theme/src/App.js
+++ react-antd-theme/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Button } from 'antd';
+
 class App extends Component {
   render() {
     return (
